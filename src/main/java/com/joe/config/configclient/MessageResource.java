a29ad048f3c39cce02ea5cb5e88ea98dd46f2e66
joseph1506 <josephin.angel@gmail.com>
2019-03-19 05:31:16 +0530

    MOre changes

---
+++ src/main/java/com/joe/config/configclient/MessageResource.java
@@ -0,0 +1,22 @@
+package com.joe.config.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/rest")
+public class MessageResource {
+
+    @Value("${message: Default Hello}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return message;
+    }
+
+}
